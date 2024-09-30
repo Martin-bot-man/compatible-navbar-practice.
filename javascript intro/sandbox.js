f941d33663515e36af91c7f937de855c710aebec
Martin Odongo <martinwere14@gmail.com>
2024-09-30 22:13:35 +0300

    used typeof in javascript

---
+++ javascript intro/sandbox.js
@@ -0,0 +1,12 @@
+const name= "Martin Owino";
+const age= 26;
+const isStudent= true;
+const address= "Nairobi";
+const hobbies= ["coding", "chess", "football"];
+const undefinedVariable = "";
+const nullVariable = "null";
+
+// console.log(typeof name);
+console.log(typeof age);
+console.log(typeof hobbies)
+ 
