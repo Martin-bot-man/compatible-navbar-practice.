daede604d5d1d2d5647c7d7786518698964fd1c3
Martin Odongo <martinwere14@gmail.com>
2024-10-01 20:24:58 +0300

    learnt if else statements

---
+++ loops/index.js
@@ -0,0 +1,20 @@
+// const number = 55;
+
+// if (number > 0){
+//     console.log(`${number} is positive`)}
+
+// else if (number< 0){
+//     console.log(`${number} is negative`)
+     
+// }else{
+//     console.log(`${number} is zero`)}
+
+// f
+// function sum(num1,num2){
+//     return num1+num2;
+// }       
+//   const result= sum(34,45) ;
+// console.log(result);
+for(let i=1; i<=10; i++){
+    console.log(i)
+}
